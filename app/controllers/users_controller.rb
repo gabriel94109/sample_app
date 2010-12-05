86f1bcb9cddf483f859599623206f215ac45246d
Gabriel Laden <gabriel94109@gmail.com>
2010-12-05 06:34:41 -0800

    User signup complete Ch8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController
+
   def new
+    @user = User.new
     @title = 'Sign up'
+  end
+
+  def create
+    @user = User.new(params[:user])  # mass assignment, not secure?
+    if @user.save
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to @user
+    else
+      @title = 'Sign up'
+      render 'new'
+    end
   end
 
   def show
