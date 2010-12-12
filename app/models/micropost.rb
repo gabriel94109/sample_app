9616bf96cb88934e0ddf5e5680d28e6da39ba906
Gabriel Laden <gabriel94109@gmail.com>
2010-12-12 13:20:03 -0800

    another fix for postgres

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -20,16 +20,13 @@ class Micropost < ActiveRecord::Base
   default_scope :order => 'microposts.created_at DESC'
   scope :from_users_followed_by, lambda { |user| followed_by(user) }
 
-  def self.from_users_followed_by(user)
-    followed_ids = user.following.map(&:id).join(', ')
-    followed_ids ||= " "
-    where("user_id IN (#{followed_ids}) OR user_id = ?", user)
-  end
-
   private
   def self.followed_by(user)
     followed_ids = %(SELECT followed_id FROM relationships WHERE follower_id = :user_id)
-    followed_ids ||= " "
-    where("user_id IN (#{followed_ids}) OR user_id = :user_id", { :user_id => user })
+    if followed_ids.count > 0
+      where("user_id IN (#{followed_ids})")
+    else
+      where("user_id = :user_id", { :user_id => user })
+    end
   end
 end
