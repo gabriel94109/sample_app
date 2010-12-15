9ce80c606dde64e0337007d136d5093edbdcfd13
Gabriel Laden <gabriel94109@gmail.com>
2010-12-15 15:37:26 -0800

    Improve User edit tabs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101208215725) do
+ActiveRecord::Schema.define(:version => 20101215224219) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -39,6 +39,9 @@ ActiveRecord::Schema.define(:version => 20101208215725) do
     t.string   "encrypted_password"
     t.string   "salt"
     t.boolean  "admin"
+    t.text     "location"
+    t.text     "website"
+    t.text     "bio"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
