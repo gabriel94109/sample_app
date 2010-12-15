c34015a40c5d249fd7af969e10bfb849648e6a1c
Gabriel Laden <gabriel94109@gmail.com>
2010-12-14 16:46:29 -0800

    Added javascript 140 char limit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function micropost_limit(){
+  var max = '140';
+  var content = $F('micropost_content');
+  if(content.length > max){
+    content = content.substr(0,max);
+  }
+  $('limit').innerHTML = max - content.length;
+}
+
+
